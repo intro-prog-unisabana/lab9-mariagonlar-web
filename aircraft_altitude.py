99813334139dc2d767305cb1081c6fc9ebbaa9b2
María Fernanda Gonzalez Lara <mariagonlar@unisabana.edu.co>
2026-04-30 17:33:20 -0500

    air4

--- aircraft_altitude.py
+++ aircraft_altitude.py
@@ -4,22 +4,13 @@ model = input("Enter aircraft model:\n")
 aircraft = Aircraft(model)
 
 while True:
-    command = input("Enter command (A for ascent, D for descent, X to exit):\n").strip()
+    command = input("Enter command (A for ascent, D for descent, X to exit):\n")
 
     if command == "X":
         break
 
-    parts = command.split()
-
-    if len(parts) < 2:
-        continue  
-
-    action = parts[0]
-
-    try:
-        feet = int(parts[1])
-    except:
-        continue  
+    action, feet = command.split()
+    feet = int(feet)
 
     if action == "A":
         aircraft.ascend(feet)
